c9ddf3361dee014a10ac298cd0f753f7eb82a96a
Daan van Berkel <daan.van.berkel@alliander.com>
2020-03-18 12:54:22 +0100

    create basic arithmetic

--- src/Fractan.elm
+++ src/Fractan.elm
@@ -1,7 +1,50 @@
 module Fractan exposing (..)
 
 import Html
+import Rational exposing (Fraction)
 
 
 main =
-    Html.text "Hello, World!"
+    let
+        f =
+            Rational.fraction 2 3
+                |> Result.withDefault Rational.one
+
+        g =
+            Rational.fraction 3 5
+                |> Result.withDefault Rational.one
+
+        h =
+            Rational.divide f g
+                |> Result.withDefault Rational.zero
+    in
+    Html.div []
+        [ Html.div []
+            [ Rational.view f
+            , Html.span [] [ Html.text "/" ]
+            , Rational.view g
+            , Html.span [] [ Html.text "=" ]
+            , Rational.view h
+            ]
+        , Html.div []
+            [ Rational.view f
+            , Html.span [] [ Html.text "*" ]
+            , Rational.view g
+            , Html.span [] [ Html.text "=" ]
+            , Rational.view <| Rational.multiply f g
+            ]
+        , Html.div []
+            [ Rational.view f
+            , Html.span [] [ Html.text "+" ]
+            , Rational.view g
+            , Html.span [] [ Html.text "=" ]
+            , Rational.view <| Rational.add f g
+            ]
+        , Html.div []
+            [ Rational.view f
+            , Html.span [] [ Html.text "-" ]
+            , Rational.view g
+            , Html.span [] [ Html.text "=" ]
+            , Rational.view <| Rational.subtract f g
+            ]
+        ]
---
+++ src/Rational.elm
@@ -0,0 +1,137 @@
+module Rational exposing (Error, Fraction, add, divide, fraction, fromInt, multiply, one, subtract, view, zero)
+
+import Html exposing (Html)
+
+
+type Fraction
+    = Fraction { numerator : Int, denominator : Int }
+
+
+one : Fraction
+one =
+    fromInt 1
+
+
+zero : Fraction
+zero =
+    fromInt 0
+
+
+fromInt : Int -> Fraction
+fromInt n =
+    Fraction { numerator = n, denominator = 1 }
+
+
+fraction : Int -> Int -> Result Error Fraction
+fraction numerator denominator =
+    if denominator /= 0 then
+        Ok <| safe_fraction numerator denominator
+
+    else
+        Err DivideByZero
+
+
+safe_fraction : Int -> Int -> Fraction
+safe_fraction p q =
+    let
+        a =
+            abs p
+
+        b =
+            abs q
+
+        s =
+            sign <| p * q
+
+        d =
+            gcd a b
+
+        numerator =
+            s * a // d
+
+        denominator =
+            b // d
+    in
+    Fraction { numerator = numerator, denominator = denominator }
+
+
+gcd : Int -> Int -> Int
+gcd a b =
+    if b == 0 then
+        a
+
+    else
+        gcd b <| modBy a b
+
+
+sign : Int -> Int
+sign n =
+    case compare n 0 of
+        LT ->
+            -1
+
+        EQ ->
+            0
+
+        GT ->
+            1
+
+
+add : Fraction -> Fraction -> Fraction
+add (Fraction f) (Fraction g) =
+    let
+        numerator =
+            f.numerator * g.denominator + f.denominator * g.numerator
+
+        denominator =
+            f.denominator * g.denominator
+    in
+    safe_fraction numerator denominator
+
+
+subtract : Fraction -> Fraction -> Fraction
+subtract (Fraction f) (Fraction g) =
+    let
+        numerator =
+            f.numerator * g.denominator - f.denominator * g.numerator
+
+        denominator =
+            f.denominator * g.denominator
+    in
+    safe_fraction numerator denominator
+
+
+multiply : Fraction -> Fraction -> Fraction
+multiply (Fraction f) (Fraction g) =
+    let
+        numerator =
+            f.numerator * g.numerator
+
+        denominator =
+            f.denominator * g.denominator
+    in
+    safe_fraction numerator denominator
+
+
+divide : Fraction -> Fraction -> Result Error Fraction
+divide (Fraction f) (Fraction g) =
+    let
+        numerator =
+            f.numerator * g.denominator
+
+        denominator =
+            f.denominator * g.numerator
+    in
+    fraction numerator denominator
+
+
+type Error
+    = DivideByZero
+
+
+view : Fraction -> Html msg
+view (Fraction { numerator, denominator }) =
+    Html.div []
+        [ Html.div [] [ Html.text <| String.fromInt numerator ]
+        , Html.div [] [ Html.text <| String.fromInt denominator ]
+        ]

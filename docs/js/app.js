266f8b1567d8c121e82070352091d0bddd397361
Daan van Berkel <daan.van.berkel@alliander.com>
2020-03-22 20:23:44 +0100

    load fractan from a description
    in the presentation a .fractan[] block with a correct json will be transformed in a fractan exploration

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,6 +2,30 @@
   let slideshow = remark.create({
     sourceUrl: 'presentation.md'
   });
+  slideshow.on('showSlide', once(function(){
+    let samples = Array.prototype.slice.call(document.getElementsByTagName('div'))
+      .filter(function(sample){ return sample.classList.contains('fractan')});
+    samples.forEach(function(sample){
+      let source = sample.getElementsByTagName('p')[0].innerText;
+      let description = JSON.parse(source);
+      let flags = { desciption: description };
+      let app = Elm.Fractan.init({
+        node: sample,
+        flags: flags
+      });
+    });
+  }));
+
+  function once(callback){
+    var called = false;
+    return function(){
+      if (!called) {
+        called = true;
+        callback();
+      }
+    }
+  }
+
  
   // Setup MathJax
   MathJax.Hub.Config({
--- docs/presentation.md
+++ docs/presentation.md
@@ -1,3 +1,14 @@
 class: middle, center
 
 # Fractan
+
+---
+
+.fractan[{
+    "number": 30,
+    "fractions": [
+        "1/2",
+        "1/3",
+        "1/5"
+    ]
+}]
